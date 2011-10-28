90ece72790fa0114bc6c750a29b47d60c82691a9
Isaku Yamahata <yamahata@private.email.ne.jp>
2011-10-28 15:55:25 +0900

    postcopy outgoing: add postcopy capability
    Added postcopy capability to migrate command with postcopy mode enabled
    
    Signed-off-by: Isaku Yamahata <yamahata@private.email.ne.jp>
    ---
    Changes v4 -> v5:
    - use migration capability instead of -p option
    
    Chnages v1 -> v2:
    - catch up for qapi change

--- include/migration/migration.h
+++ include/migration/migration.h
@@ -142,6 +142,8 @@ void migrate_add_blocker(Error *reason);
  */
 void migrate_del_blocker(Error *reason);
 
+bool migration_postcopy_outgoing(void);
+
 bool migrate_rdma_pin_all(void);
 bool migrate_zero_blocks(void);
 
