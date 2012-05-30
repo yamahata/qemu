861b3ba024c1ed35763657e635a006818fdb910c
Isaku Yamahata <yamahata@valinux.co.jp>
2012-05-30 12:05:57 +0900

    arch_init: factor out setting last_block, last_offset
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- arch_init.h
+++ arch_init.h
@@ -50,6 +50,7 @@ void ram_save_page_reset(void);
 int ram_load_page(QEMUFile *f, void *host, int flags);
 
 #if defined(NEED_CPU_H) && !defined(CONFIG_USER_ONLY)
+void ram_save_set_last_block(RAMBlock *block, ram_addr_t offset);
 bool ram_save_page(QEMUFile *f, RAMBlock *block, ram_addr_t offset,
                    bool last_stage);
 RAMBlock *ram_find_block(const char *id, uint8_t len);
