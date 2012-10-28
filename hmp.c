248767a0bc6d65d5f8a739f3781c08c577bfaef5
Isaku Yamahata <yamahata@private.email.ne.jp>
2012-10-28 17:29:05 +0900

    postcopy/outgoing: introduce precopy_count parameter
    Precopy with this loop number before postcopy mode.
    This will be implemented by the next patch.
    
    Signed-off-by: Isaku Yamahata <yamahata@private.email.ne.jp>

--- hmp.c
+++ hmp.c
@@ -1213,12 +1213,14 @@ void hmp_migrate(Monitor *mon, const QDict *qdict)
     int detach = qdict_get_try_bool(qdict, "detach", 0);
     int blk = qdict_get_try_bool(qdict, "blk", 0);
     int inc = qdict_get_try_bool(qdict, "inc", 0);
+    int precopy_count = qdict_get_try_int(qdict, "precopy_count", 0);
     int forward = qdict_get_try_int(qdict, "forward", 0);
     int backward = qdict_get_try_int(qdict, "backward", 0);
     const char *uri = qdict_get_str(qdict, "uri");
     Error *err = NULL;
 
     qmp_migrate(uri, !!blk, blk, !!inc, inc, false, false,
+                !!precopy_count, precopy_count,
                 !!forward, forward, !!backward, backward,
                 &err);
     if (err) {
