d1a76b4b22d4af28d2cbfdcc2199ba78a233f4b1
Isaku Yamahata <yamahata@private.email.ne.jp>
2013-07-16 18:32:59 +0900

    exec: export qemu_get_safe_ram_ptr()
    Later postcopy incoming part will use it.
    
    Signed-off-by: Isaku Yamahata <yamahata@private.email.ne.jp>

--- include/exec/memory-internal.h
+++ include/exec/memory-internal.h
@@ -37,6 +37,7 @@ ram_addr_t qemu_ram_alloc_from_ptr(ram_addr_t size, void *host,
                                    MemoryRegion *mr);
 ram_addr_t qemu_ram_alloc(ram_addr_t size, MemoryRegion *mr);
 void *qemu_get_ram_ptr(ram_addr_t addr);
+void *qemu_safe_ram_ptr(ram_addr_t addr);
 void qemu_ram_free(ram_addr_t addr);
 void qemu_ram_free_from_ptr(ram_addr_t addr);
 
